01d359ea30d8c53d7cfdd6323f6179c4c009dfdc
xmy <770212572@qq.com>
2018-04-27 16:53:29 +0800

    create socket

---
+++ src/main/java/com/xmy/toursocket/TourSocketApplication.java
@@ -0,0 +1,12 @@
+package com.xmy.toursocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourSocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourSocketApplication.class, args);
+	}
+}
